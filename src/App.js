69dd187c80b9d3d7b01e1ce0be29103f20423a39
AminSaedi <amin.di.sam@gmail.com>
2021-05-04 16:39:23 +0430

    Create BurgerBuilder container

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import "./App.css";
-import Layout from './components/Layout/Layout';
+import Layout from "./components/Layout/Layout";
+import BurgerBuilder from "./containers/BurgerBuilder";
 
 function App() {
   return (
     <Layout>
-      <p>سلام خوشمزه</p>
+      <BurgerBuilder />
     </Layout>
   );
 }
